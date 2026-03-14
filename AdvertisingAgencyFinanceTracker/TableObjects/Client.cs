b7c4061b45eb91ed78dc2f3e2b19bc471d4684e7
nukai0 <m.sarichev1@gmail.com>
2026-03-14 12:49:19 +0300

    Created new folder "TableObjects" and added new classes

---
+++ AdvertisingAgencyFinanceTracker/TableObjects/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisingAgencyFinanceTracker.TableObjects
+{
+    internal class Client
+    {
+        public int Id { get; set; }
+        public string Company_name { get; set; }
+        public string Contact_person { get; set; }
+        public int Manager_id { get; set; }
+    }
+}
---
+++ AdvertisingAgencyFinanceTracker/TableObjects/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisingAgencyFinanceTracker.TableObjects
+{
+    internal class Expense
+    {
+        public int Id { get; set; }
+        public int Invoice_id { get; set; }
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+    }
+}
