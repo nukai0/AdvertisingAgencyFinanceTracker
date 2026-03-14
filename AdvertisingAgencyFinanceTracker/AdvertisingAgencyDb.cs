9f0d1b259b93fc036839b49e45db35b1d57b72f5
nukai0 <m.sarichev1@gmail.com>
2026-03-14 14:51:34 +0300

    Create architecture, added new nugget packages

---
+++ AdvertisingAgencyFinanceTracker/AdvertisingAgencyDb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisingAgencyFinanceTracker
+{
+    internal class AdvertisingAgencyDb
+    {
+        private static readonly string connectionString = "Host=localhost;Port=5433;Database=AdvertisingAgencyDb;Username=postgres;Password=1234";
+    }
+}
---
+++ AdvertisingAgencyFinanceTracker/AdvertisingAgencyRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+
+namespace AdvertisingAgencyFinanceTracker
+{
+    internal class AdvertisingAgencyRepository : IAdvertisingAgencyRepository
+    {
+        string connection;
+        public AdvertisingAgencyRepository(string connection)
+        {
+            this.connection = connection;
+        }
+    }
+}
